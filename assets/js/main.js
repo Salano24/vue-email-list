32f7dfd36c2cbeb70a682f8a5b1dce30bc775b5d
Salano24 <salernomarco48@email.com>
2022-11-08 15:07:01 +0100

    fine esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+let container = document.querySelector(".container");
+const {
+    createApp 
+} = Vue
+
+createApp({
+    data(){
+        return{
+      
+          emails: [] 
+          
+        }
+    },
+    mounted() {
+      for (let i = 0; i < 10; i++) {
+            axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response => {
+          this.emails.push(response.data.response);
+
+  
+        });             
+        }
+        console.log(this.emails);
+
+      
+    }
+    
+}).mount('#app')
